749b37277812b283632e61e67bfb8ba5c3be1f99
diego <diegopgcds10@gmail.com>
2021-11-25 19:49:32 -0300

    projeto-usuarios

---
+++ projeto-usuarios/index.js
@@ -0,0 +1,18 @@
+let userController = new UserController("form-user-create", "form-user-update", "table-users");
+
+
+
+                                                                //35 re  
+                                                                //37 re
+                                                                //39 re
+                                                                //41 re
+                                                                //47 re
+                                                                //48 - 53 re
+
+                                                                //objeto não esta funcioando
+                                                                //layout esta torto
+
+                                                                //Model View Controller = MVC - Estrutura do projeto em camadas
+                                                                //Model é onde os dados são armanezados
+                                                                //View é oque eu mostro para o usuário, é a parte que ele utiliza
+                                                                //Controller liga o Model e a View, e cria as regras de negocio
